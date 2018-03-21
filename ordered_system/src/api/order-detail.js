fd16003ac5b215c1a39d2fea2287d8a6436f47b1
sxw <18268345690@163.com>
2018-03-21 16:55:12 +0800

    3.21

---
+++ ordered_system/src/api/order-detail.js
@@ -0,0 +1,17 @@
+import fetch from 'common/js/util.js'
+import url from 'common/js/config.js'
+export function getOrderDetail(ID){
+    return fetch.get(url+"/getroombyid?roomId="+ID)
+}
+export function getstatebyMEET(OBJ){
+    return fetch.post(url+"/getstatebymeetidanddate",OBJ)
+}
+export function getUserCont(CODE){
+    return fetch.get(url+"/wechart?code="+CODE)
+}
+export function getinsertmeeting(MSG){
+    return fetch.post(url+'/insertmeeting',MSG)
+}
+export function StateController(ID){
+    return fetch.get(url+'/getStateByMeetIdForDate'+'?stateRoomId='+ID)
+} 
---
+++ ordered_system/src/common/js/config.js
@@ -0,0 +1,2 @@
+const url="http://118.178.94.113:92/meet-wx3"
+export default url;
